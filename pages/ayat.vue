74542338c9feba4dfcb2f8533bba159f0839a6d7
Ridwan Maulana <rdwanf49@gmail.com>
2021-05-30 19:50:14 +0700

    update: responsive layout

--- pages/ayat.vue
+++ pages/ayat.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container">
-            <h3 class="bg-white text-black py-4 px-4 rounded-md shadow font-medium border border-red-600"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <h3 class="bg-white text-black py-4 px-4 m-4 rounded-md shadow font-medium border border-red-600"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg> Maaf halaman ini masih dalam pengembangan
             <br>
--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,16 @@
   <div class="container">
     <Nav />
     <div>
-      <h3 class="bg-gray-700 py-9 px-4 rounded-md m-4 text-white font-semibold border border-blue-700">Aplikasi Qur'an terjemahan Bahasa Indonesia lengkap dengan Tafsir</h3>
+      <h3 class="bg-gray-800 py-9 px-4 rounded-md m-4 text-white font-semibold shadow-md">Aplikasi Qur'an terjemahan Bahasa Indonesia lengkap dengan Tafsir</h3>
       <br>
       <hr>
         <p class="text-left m-4 font-medium">List Surah :</p>
-        <div v-for="surah in surah.data" :key="surah.id" class="h-auto text-left w-auto py-6 px-4 m-4 rounded-lg shadow text-black border">
+        <div v-for="surah in surah.data" :key="surah.id" class="h-auto text-left w-auto py-6 px-4 m-4 rounded-lg hover:shadow-lg text-black border">
           <ul>
             <li><p>Nama Surah : {{surah.name.transliteration.id}}</p></li>
             <li><p>Diturunkan : {{surah.revelation.id}}</p></li>
             <div class="dropdown">
-              <button class="bg-gray-300 shadow-sm rounded-md text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
+              <button class="bg-gray-300 shadow-md hover:shadow-none rounded-md text-gray-700 font-semibold py-2 px-4 rounded inline-flex items-center">
                 <span class="mr-1">Tafsir</span>
                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/> </svg>
               </button>
@@ -19,7 +19,7 @@
                 <li class=""><a class="rounded-md bg-gray-200 hover:bg-gray-400 py-2 px-4 block whitespace-no-wrap">{{surah.tafsir.id}}</a></li>
               </ul>
               <button @click="$router.push({name: 'surah-id', params:{id:surah.number},})"
-                class="button-shadow bg-blue-600 text-white rounded-md font-normal py-2 px-4 rounded-md"
+                class="bg-blue-600 text-white shadow-md hover:shadow-none rounded-md font-normal py-2 px-4 rounded-md"
               >
                 Baca surah
               </button>
