4f80d30007648ceaca4d8af899b067c938f269e0
Ridwan Maulana <rdwanf49@gmail.com>
2021-06-01 08:49:24 +0700

    update: title header

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'quran-ID',
+    title: 'Aplikasi Baca Al-Quran praktis dari browser | Quran-ID.vercel.app',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
