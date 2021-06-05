887c10a3585ce63025d40715349b2870fd5be862
Ridwan Maulana <rdwanf49@gmail.com>
2021-06-05 10:30:54 +0700

    add: page daily doa

---
+++ pages/dailydoa.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container">
+            <h3 class="bg-white text-black py-4 px-4 m-4 rounded-md shadow font-medium border border-red-600"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 inline text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+            </svg> Maaf halaman ini masih dalam pengembangan
+            <br>
+            <nuxt-link to="/">
+                <p class="text-red-500 pt-3"><svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 inline" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
+                </svg> back to home</p>
+            </nuxt-link>
+            </h3>
+    </div>
+</template>
