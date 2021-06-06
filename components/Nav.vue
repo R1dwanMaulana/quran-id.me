ed4c93d95c04f95a6d4a24e5fcaf25052eb3264e
Ridwan Maulana <rdwanf49@gmail.com>
2021-06-06 19:03:05 +0700

    chore: fix link nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -13,7 +13,7 @@
 </svg>
             Doa Harian
         </nuxt-link>
-        <a href="https://riddev.tech" target="_blank" class="w-full block py-2 px-3 text-center hover:bg-green-700 transition duration-300">
+        <a href="/about" class="w-full block py-2 px-3 text-center hover:bg-green-700 transition duration-300">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-6 mb-1 mx-auto" viewBox="0 0 20 20" fill="currentColor">
   <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
 </svg>
