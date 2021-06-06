175cb48c549d1cd667cdd842e5014a85b18f70db
Ridwan Maulana <rdwanf49@gmail.com>
2021-06-06 19:05:13 +0700

    add: about pages

--- pages/about.vue
+++ pages/about.vue
@@ -40,7 +40,7 @@
             <!-- </div> -->
 
             <p class="font-light mt-20 text-sm">Contents licensed under <span class="text-green-600"><a href="https://creativecommons.org/licenses/by-nc-sa/4.0/deed.id" target="_blank">CC BY-NC-SA 4.0.</a></span></p>
-            <p class="font-light text-sm">made using <span class="text-green-600"><a href="https://nuxtjs.org/" target="_blank">Nuxt.js</a></span> and  <span class="text-green-600"><a href="https://tailwindcss.com/" target="_blank">Tailwind CSS.</a></span> Hosted on <span class="text-green-600"><a href="https://vercel.com" target="_blank">Vercel.</a></span></p>
+            <p class="font-light text-sm">Made using <span class="text-green-600"><a href="https://nuxtjs.org/" target="_blank">Nuxt.js</a></span> and  <span class="text-green-600"><a href="https://tailwindcss.com/" target="_blank">Tailwind CSS.</a></span> Hosted on <span class="text-green-600"><a href="https://vercel.com" target="_blank">Vercel.</a></span></p>
             <p class="font-light mt-3 text-sm">MIT License © 2021 - present | <span class="text-green-600"><a href="https://riddev.tech" target="_blank">Ridwan Maulana.</a></span></p>
         </div>
     </div>
