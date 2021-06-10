22ad5664378eaba72f79bd84b2dbd4c3352805df
Ridwan Maulana <rdwanf49@gmail.com>
2021-06-10 19:20:31 +0700

    update: improve font size & letter spacing arabic

--- pages/surah/_id.vue
+++ pages/surah/_id.vue
@@ -4,7 +4,7 @@
       <div>
         <div v-for="verse in surah.data.verses" :key="verse.text.arab" class="bg-gray-900 border border-transparent py-5 px-4 rounded-xl m-4 text-right">
           <div class="object-left-top py-1 pr-4 pl-2 w-10 rounded-md bg-gray-700 text-white font-medium">{{verse.number.inSurah}}</div>
-          <h3 class="arabic pt-2 pb-3 text-white">{{verse.text.arab}}</h3>
+          <h3 class="arabic pt-2 pb-3 text-white tracking-wide">{{verse.text.arab}}</h3>
           <h3 class="text-left text-yellow-600">{{verse.text.transliteration.en}}</h3>
           <!-- text-yellow-600 bg-gray-800 rounded-md py-3 px-3 -->
           <h3 class="font-medium text-left text-white"><i>{{verse.translation.id}}</i></h3>
@@ -24,7 +24,7 @@ export default {
 
 <style scoped>
 .arabic {
-  font-size: 25px;
+  font-size: 30px;
   font-family:'Times New Roman', Times, serif
 }
 </style>
