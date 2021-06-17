8341737af65a1ff8286ca88cba864760a6aaf639
Ridwan Maulana <rdwanf49@gmail.com>
2021-06-17 20:07:04 +0700

    add: header navigation

--- components/HeaderNav.vue
+++ components/HeaderNav.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="navbar-top fixed top-0 inset-x-0 text-white text-xs uppercase font-serif flex jestify-between">
+    <nav class="navbar-top fixed top-0 inset-x-0 text-white text-xs uppercase font-serif flex justify-between">
         <nuxt-link to="/" class="w-full block py-4 px-5 text-left">
 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
