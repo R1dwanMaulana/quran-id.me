e8c99fda742a9e58a3c595418d859b8710a31892
Ridwan Maulana <rdwanf49@gmail.com>
2021-09-08 07:03:05 +0700

    feat rename text button

--- pages/index.vue
+++ pages/index.vue
@@ -28,7 +28,7 @@
             </div>
             <div class="dropdown inline">
               <button @click="clickShow(surah.number)" class="bg-gray-200 focus:outline-none shadow-sm hover:shadow-none rounded-md text-gray-700 font-normal py-2 px-4 rounded inline-flex items-center">
-                <span class="mr-1">Tafsir</span>
+                <span class="mr-1">Muqaddimah</span>
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                 </svg>
