c4125c111ae7ff4f0225151979583a6f40c7e26c
Ridwan Maulana <rdwanf49@gmail.com>
2021-09-09 06:50:19 +0700

    feat: configure SSR & SEO

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,17 +1,29 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+  target: 'server',
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Aplikasi Baca Al-Quran praktis dari browser | Quran-ID.vercel.app',
+    htmlAttrs: {
+			lang: 'id'
+		},
+    title: 'Quran-id - Aplikasi Baca Al-Quran praktis dari browser | quran-id.vercel.app',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '' },
+      { name: "format-detection", content: "telephone=no" },
+			{ name: "author", content: "Quran-id"},
+			{ name: "description", content: "Quran-id adalah aplikasi baca al-quran dari web browser lengkap dengan tafsir"},
+			{ property: "og:title", content: "Quran-id - Aplikasi Baca Al-Quran praktis dari browser"},
+			{ property: "og:description", content: "Quran-id adalah aplikasi baca al-quran dari web browser lengkap dengan tafsir."},
+			{ property: "og:image", content: "/icon.png"},
+			{ property: "og:site_name", content: "Quran-id"},
+			{ property: "og:type", content: "website"},
+			{ property: "og:url", content: "https://quran-id.vercel.app/"}
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/quran.ico' }
@@ -21,6 +33,9 @@ export default {
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
+  loading: {
+    color: '#F87171'
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -51,7 +66,7 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: 'id'
     }
   },
 
