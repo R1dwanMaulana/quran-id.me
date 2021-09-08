d1213bcb49a34d4c9dbfcca31b5f2a69e755f174
Ridwan Maulana <rdwanf49@gmail.com>
2021-09-08 10:39:47 +0700

    feat: add tafsir ayat

--- pages/surah/_id.vue
+++ pages/surah/_id.vue
@@ -3,20 +3,33 @@
     <HeaderNav />
       <div class="pt-16">
         <!-- <div v-for="verse in surah.data.verses" :key="verse.text.arab"> -->
-        <div v-for="verse in surah.data.verses" :key="verse.text.arab" class="bg-gray-900 border border-transparent py-5 px-4 rounded-xl m-4 text-right">
+        <div v-for="verse in surah.data.verses" :key="verse" class="bg-gray-900 border border-transparent py-5 px-4 rounded-xl m-4 text-right">
           <div class="object-left-top py-1 pr-4 pl-2 w-10 rounded-md bg-gray-700 text-white font-medium">{{verse.number.inSurah}}</div>
           <h3 class="arabic pt-2 pb-3 text-white leading-relaxed">{{verse.text.arab}}</h3>
           <h3 class="text-left text-yellow-600">{{verse.text.transliteration.en}}</h3>
 
           <div class="text-left">
+              <div @click="clickShowTafsir(verse.tafsir)" class="focus:outline-none font-normal inline-flex items-center py-2">
+                <span class="mr-1 text-gray-400">Tafsir</span>
+                <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                  <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg> -->
+                <img :src="toogleIcon" @click="icon = !icon" alt="">
+              </div>
+
               <div @click="clickShow(verse.text.arab)" class="focus:outline-none font-normal inline-flex items-center py-2">
                 <span class="mr-1 text-gray-400">Arti</span>
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                <!-- <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
                   <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                </svg>
+                </svg> -->
+                <img :src="toogleIcon2" @click="icon2 = !icon2" alt="">
               </div>
               <ul v-show="showTranslate[verse.text.arab]" class="pb-3 text-gray-700 pt-1">
                 <li><a class="bg-transparent text-white py-2 block whitespace-no-wrap">{{verse.translation.id}}</a></li>
+              </ul>
+              <ul v-show="showTafsir[verse.tafsir]" class="pb-3 text-gray-700 pt-1">
+                <li class="text-white font-semibold">Tafsir dari kemenang:</li>
+                <li><a class="bg-transparent text-white py-2 block whitespace-no-wrap">{{verse.tafsir.id.long}}</a></li>
               </ul>
           </div>
           <!-- text-yellow-600 bg-gray-800 rounded-md py-3 px-3 -->
@@ -30,7 +43,10 @@
 export default {
   data() {
     return {
-      showTranslate: {}
+      showTranslate: {},
+      showTafsir: {},
+      icon: false,
+      icon2: false,
     }
   },
   async asyncData({$axios, params}) {
@@ -54,6 +70,38 @@ export default {
         }
       }
       console.log('after', this.showTranslate)
+    },
+
+    clickShowTafsir(id) {
+      console.log('before', this.showTafsir)
+      if(this.showTafsir[id]) {
+        this.showTafsir = {
+          ...this.showTafsir, 
+          [id]: false
+        }
+      } else {
+        this.showTafsir = {
+          ...this.showTafsir, 
+          [id]: true
+        }
+      }
+      console.log('after', this.showTafsir)
+    }
+  },
+  computed: {
+    toogleIcon() {
+      if(this.icon) {
+        return require('/static/arrow-up.svg')
+      } else {
+        return require('/static/arrow-down.svg')
+      }
+    },
+    toogleIcon2() {
+      if(this.icon2) {
+        return require('/static/arrow-up.svg')
+      } else {
+        return require('/static/arrow-down.svg')
+      }
     }
   }
 }
