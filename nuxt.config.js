afd174ab393f3beab437fbbe454ca9d8c19ee5a3
Ridwan Maulana <rdwanf49@gmail.com>
2021-09-14 09:42:07 +0700

    feat: add tag meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,9 @@ export default {
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: ["~/plugins/analytics.js"],
+  plugins: [
+    
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
