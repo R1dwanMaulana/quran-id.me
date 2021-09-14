4d8a35d6982512f5022d9763e1fb4953025ed8ec
Ridwan Maulana <rdwanf49@gmail.com>
2021-09-14 08:54:56 +0700

    feat: config GA4

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,8 +28,7 @@ export default {
   },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: ["~/plugins/analytics.js"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
