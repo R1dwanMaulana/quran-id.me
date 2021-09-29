04158868a65653404d3b66715d091d6bdd050218
Ridwan Maulana <rdwanf49@gmail.com>
2021-09-29 10:59:59 +0700

    choreupdate nav

--- components/Nav.vue
+++ components/Nav.vue
@@ -12,13 +12,13 @@
 </svg>
             Doa Tahlil
         </nuxt-link>
-        <nuxt-link to="/dailydoa" class="nav-link w-full block py-2 px-2 text-center hover:bg-gray-800 transition duration-300">
+        <!-- <nuxt-link to="/dailydoa" class="nav-link w-full block py-2 px-2 text-center hover:bg-gray-800 transition duration-300">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-6 mb-1 mx-auto" viewBox="0 0 20 20" fill="currentColor">
   <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
   <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd" />
 </svg>
             Doa Harian
-        </nuxt-link>
+        </nuxt-link> -->
         <nuxt-link to="/about" class="nav-link w-full block py-2 px-2 text-center hover:bg-gray-800 transition duration-300">
             <svg xmlns="http://www.w3.org/2000/svg" class="w-7 h-6 mb-1 mx-auto" viewBox="0 0 20 20" fill="currentColor">
   <path fill-rule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
